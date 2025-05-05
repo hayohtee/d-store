28a510cd8376fb2907d8e5305b71b4b8b8703802
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-05-05 13:56:27 +0100

    Create errors.go

---
+++ cmd/api/errors.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+func (app *application) logError(r *http.Request, err error) {
+	app.logger.Error(err.Error(), slog.String("request_method", r.Method), slog.String("request_url", r.URL.String()))
+}
+
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message string) {
+	env := envelope{"error": message}
+	if err := writeJSON(w, status, env, nil); err != nil {
+		app.logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func (app *application) internalServerErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+	message := "the server encountered a problem and cannot process the request"
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
