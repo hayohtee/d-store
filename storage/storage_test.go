d37ca8b796a0d952885d8273a1071a1cd3c529d8
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-05-05 12:40:27 +0100

    Add TestDelete()

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -27,11 +27,11 @@ var tt = []struct {
 func TestPut(t *testing.T) {
 	for _, tc := range tt {
 		if err := Put(tc.key, tc.value); err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 
-		value, exists := store[tc.key]
-		if !exists {
+		value, exist := store[tc.key]
+		if !exist {
 			t.Errorf("put failed: expected %q to be in the store", tc.key)
 		}
 
@@ -39,7 +39,6 @@ func TestPut(t *testing.T) {
 			t.Errorf("value mismatch: expected %q, but got %q", tc.value, value)
 		}
 	}
-
 	clear(store)
 }
 
@@ -51,11 +50,30 @@ func TestGet(t *testing.T) {
 
 		value, err := Get(tc.key)
 		if err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 
 		if value != tc.value {
 			t.Errorf("value mismatch: expected %q, but got %q", tc.value, value)
 		}
 	}
+	clear(store)
+}
+
+func TestDelete(t *testing.T) {
+	for _, tc := range tt {
+		if err := Put(tc.key, tc.value); err != nil {
+			t.Fatal(err)
+		}
+		
+		if err := Delete(tc.key); err != nil {
+			t.Fatal(err)
+		}
+		
+		_, exists := store[tc.key]
+		if exists {
+			t.Errorf("delete failed: %q key should not exist", tc.key)
+		}
+	}
+	clear(store)
 }
