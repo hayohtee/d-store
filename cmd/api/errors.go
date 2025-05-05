2976af7b905eda7ff37e7b8c80a2aa9077d8b07b
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-05-05 14:11:44 +0100

    Add notFoundResponse()

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -30,3 +30,8 @@ func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Reques
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the requested resource cannot be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
+}
